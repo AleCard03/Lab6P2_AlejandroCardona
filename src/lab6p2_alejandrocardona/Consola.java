33346cd81c7263a3fec591ea58b22838126698f7
Alejandro Cardona <alexcardona3002@gmail.com>
2023-08-25 13:32:17 -0600

    Commit 2 - Creacion de Clases y Algunos Atributos

---
+++ src/lab6p2_alejandrocardona/Consola.java
@@ -0,0 +1,14 @@
+
+package lab6p2_alejandrocardona;
+
+import java.util.ArrayList;
+
+public class Consola {
+    
+    private String ID, fabricante;
+    private int years , cantJuegos;
+    private ArrayList<Juego> juegos;
+    private double precio;
+    private String modelo;
+    
+}
